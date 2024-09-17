80ce263e51407acb62a3ceb5abf504f3a48cb3af
Paul Santus <paul.santus@gmail.com>
2024-09-17 17:30:22 +0200

    Support for SQS

--- Terraform/lambda.tf
+++ Terraform/lambda.tf
@@ -16,6 +16,63 @@ resource "aws_lambda_function" "sample_php_lambda_apigw" {
       APP_DEBUG =  "0"
       APP_SECRET = "2ca64f8d83b9e89f5f19d672841d6bb8"
       DYNAMODB_CACHE_TABLE = aws_dynamodb_table.bref_cache.name
+      MESSENGER_TRANSPORT_DSN = aws_sqs_queue.queue.url
+    }
+  }
+
+  vpc_config {
+    security_group_ids = [aws_security_group.lambda.id]
+    subnet_ids = data.aws_subnets.default.ids
+  }
+}
+
+resource "aws_lambda_function" "worker" {
+  function_name    = "php-bref-demo-symfony-app-worker"
+  role             =  aws_iam_role.iam_for_lambda.arn
+  runtime          = "provided.al2"
+  handler          = "bin/consumer.php"
+  timeout          = 28
+  filename         = data.archive_file.zip_php_lambda.output_path
+  source_code_hash = data.archive_file.zip_php_lambda.output_base64sha256
+  publish          = false
+  memory_size      = 1024
+  layers = ["arn:aws:lambda:eu-west-1:534081306603:layer:php-83-fpm:31"] # See https://bref.sh/docs/runtimes/runtimes-details
+  environment {
+    variables = {
+      APP_ENV = "prod"
+      APP_DEBUG =  "0"
+      APP_SECRET = "2ca64f8d83b9e89f5f19d672841d6bb8"
+      MESSENGER_TRANSPORT_DSN = aws_sqs_queue.queue.url
+      DATABASE_URL = "mysql://${aws_rds_cluster.db.master_username}:${aws_rds_cluster.db.master_password}@${aws_rds_cluster.db.endpoint}:3306/${aws_rds_cluster.db.database_name}?serverVersion=8&charset=utf8mb4"
+      DYNAMODB_CACHE_TABLE = aws_dynamodb_table.bref_cache.name
+    }
+  }
+
+  vpc_config {
+    security_group_ids = [aws_security_group.lambda.id]
+    subnet_ids = data.aws_subnets.default.ids
+  }
+}
+
+resource "aws_lambda_function" "console" {
+  function_name    = "php-bref-demo-symfony-app-console"
+  role             =  aws_iam_role.iam_for_lambda.arn
+  runtime          = "provided.al2"
+  handler          = "bin/console"
+  timeout          = 28
+  filename         = data.archive_file.zip_php_lambda.output_path
+  source_code_hash = data.archive_file.zip_php_lambda.output_base64sha256
+  publish          = false
+  memory_size      = 1024
+  layers = ["arn:aws:lambda:eu-west-1:534081306603:layer:php-83-fpm:31"] # See https://bref.sh/docs/runtimes/runtimes-details
+  environment {
+    variables = {
+      APP_ENV = "prod"
+      APP_DEBUG =  "0"
+      APP_SECRET = "2ca64f8d83b9e89f5f19d672841d6bb8"
+      MESSENGER_TRANSPORT_DSN = aws_sqs_queue.queue.url
+      DATABASE_URL = "mysql://${aws_rds_cluster.db.master_username}:${aws_rds_cluster.db.master_password}@${aws_rds_cluster.db.endpoint}:3306/${aws_rds_cluster.db.database_name}?serverVersion=8&charset=utf8mb4"
+      DYNAMODB_CACHE_TABLE = aws_dynamodb_table.bref_cache.name
     }
   }
 
@@ -29,6 +86,9 @@ resource "aws_lambda_function" "sample_php_lambda_apigw" {
 data "archive_file" "zip_php_lambda" {
   type        = "zip"
   source_dir  = "../SymfonyApp"
+  excludes    = [
+    "tests",
+  ]
   output_path = "app.zip"
 }
 
---
+++ Terraform/sqs.tf
@@ -0,0 +1,23 @@
+resource "aws_sqs_queue" "queue" {
+  name = "php-bref-demo-symfony-app-queue"
+}
+
+resource "aws_sqs_queue_policy" "queue_policy" {
+  policy    = data.aws_iam_policy_document.queue_policy.json
+  queue_url = aws_sqs_queue.queue.url
+}
+
+data "aws_iam_policy_document" "queue_policy" {
+  statement {
+    sid    = "First"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.iam_for_lambda.arn]
+    }
+
+    actions   = ["sqs:*"]
+    resources = [aws_sqs_queue.queue.arn]
+  }
+}
